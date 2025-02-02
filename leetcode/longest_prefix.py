1b543f22e753a3d31b63f84b48b5b0df94701946
Daniel Steman <daniel-steman@live.nl>
2025-02-02 13:57:46 +0100

    fix prefix

--- leetcode/longest_prefix.py
+++ leetcode/longest_prefix.py
@@ -1,2 +1,17 @@
-strs = ["flower","flow","flight"]
-print(min(strs))
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        longest_common_prefix = ""
+        shortest_str = min(strs, key=len)
+        for i in range(len(shortest_str)):
+            char = shortest_str[i]
+            if all([x[i] == char for x in strs]):
+                longest_common_prefix += char
+            else:
+                return longest_common_prefix
+
+        return longest_common_prefix
+
+
+if __name__ == "__main__":
+    print(Solution().longestCommonPrefix(["flower", "flow", "flight"]))
+    print(Solution().longestCommonPrefix(["cir", "car"]))
